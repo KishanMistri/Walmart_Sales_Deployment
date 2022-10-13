c073ad27f99e2356c10cf6d5b489eb42330396e7
Kishan Mistri <14bit029@nirmauni.ac.in>
2022-10-13 22:47:18 +0530

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title('Walmart Unit Sales Forecasting')
+
+# data = "./data/police.csv"
+# weather_data = "./weather.csv"
+
+# @st.cache
+# def load_data(path,nrows=10000):
+#     data = pd.read_csv(path, nrows=nrows)
+# #     lowercase = lambda x: str(x).lower()
+# #     data.rename(lowercase, axis='columns', inplace=True)
+# #     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     return data
+  
