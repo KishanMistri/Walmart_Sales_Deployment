9bc22803126cb8302338e1245726520747d88664
Kishan Mistri <14bit029@nirmauni.ac.in>
2022-10-16 16:55:59 +0530

    Update init_script.sh

--- build-infra/scripts/init_script.sh
+++ build-infra/scripts/init_script.sh
@@ -12,14 +12,13 @@ echo aws --version
 # Pip and python version
 sudo apt install python3-pip -y
 sudo apt install pip -y
-sudo apt-get install python3.7 -y
+sudo apt-get install python3.9 -y
    
 curl "https://bootstrap.pypa.io/get-pip.py" -o "/tmp/get-pip.py"
 python3 /tmp/get-pip.py --user
 echo "export PATH=/home/ubuntu/.local/bin:$PATH" | cat >> ~/.bash_profile
-source ~/.bash_profile
+. ~/.bash_profile
 
 # Cleanup
 rm -rf /tmp/get-pip.py
 rm -rf /tmp/awscliv2.zip
-
